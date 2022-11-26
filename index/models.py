dfb30dc9334dc886f6bb97d0d615c1980c059833
ke-stack <kingkevo254@gmail.com>
2022-11-26 21:38:36 +0300

    added doctor object

--- index/models.py
+++ index/models.py
@@ -15,6 +15,19 @@ class User(db.Model):
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
 
+class Doctor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.Integer, unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    appointments = db.relationship('Appointment', backref='patient', lazy=True)
+
+
+    def __repr__(self):
+        return f"User('{self.username}','{self.email}','{self.image_file}')"
+
 class Appointment(db.Model):
     id = db.Column(db.Integer, primary_key= True)
     doctors_note = db.Column(db.String(1000), nullable=False)
