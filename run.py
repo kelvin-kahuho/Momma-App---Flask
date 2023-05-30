a8b6da5b9d4a461423fa65a92b82997d56070f45
ke-stack <kingkevo254@gmail.com>
2023-05-30 23:50:57 +0300

    Dropped all database items and created new tables

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from index import app, db
 
 
 if __name__ == '__main__':
+    db.drop_all()
     db.create_all()
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="localhost", port=5003, debug=True)
