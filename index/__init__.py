0114e87f4143bffe2e6ce1cc426d6e3f745ae973
ke-stack <kingkevo254@gmail.com>
2023-06-04 11:08:57 +0300

    Updated the init file after trying out different thing likes db migrations- this file is in its best version

--- index/__init__.py
+++ index/__init__.py
@@ -8,14 +8,12 @@ app = Flask(__name__, static_url_path='/index', static_folder='static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 app.config['SECRET_KEY'] = '788d5501a3787d2ddfe537f2f40239f3'
 
-
-from index.models import User, Appointment
+from index.models import User, Appointment, Doctor, Timeslot
 admin = Admin(app, name='Admin Interface')
 admin.add_view(ModelView(User, db.session))
 admin.add_view(ModelView(Appointment, db.session))
-
-
 
 from index import routes
