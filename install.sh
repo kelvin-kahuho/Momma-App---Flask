41a1fa908d160a6649081d2ca6667139961145c0
ke-stack <kingkevo254@gmail.com>
2023-06-05 21:05:05 +0300

    Adding an install and run script in home dir

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Installing  flask
+pip install flask
+
+# Installing Rasa
+pip install rasa
+
+#Installing flask_sqlalchemy
+pip install flask_sqlalchemy
+
+#Installing flask_admin
+pip install flask_admin
