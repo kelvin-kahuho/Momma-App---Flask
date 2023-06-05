15b568c517ea552e05a77f136a743f293a046337
ke-stack <kingkevo254@gmail.com>
2023-06-05 21:43:38 +0300

    Modified libraries to be installed to specific versions for compactibility reasons

--- install.sh
+++ install.sh
@@ -7,7 +7,10 @@ pip install flask
 pip install rasa
 
 #Installing flask_sqlalchemy
-pip install flask_sqlalchemy
+pip install flask_sqlalchemy==2.5.1
 
 #Installing flask_admin
 pip install flask_admin
+
+#Installing SQLAlchemy==1.4.4
+pip install SQLAlchemy==1.4.4
